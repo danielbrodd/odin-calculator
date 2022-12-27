dafa8c4f3205a8aaf7e13131dcdc4bed9ff95ca8
danielbrodd <daniel.brodd@gmail.com>
2022-12-27 22:30:38 +0100

    Optimize js
    Realized I didn't need seperate querySelectors and eventlistners for
    each .numbers button.

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ const bDiv = document.querySelectorAll('#bDiv')
 const allNumbers = document.querySelectorAll('.numbers')
 
 function buttonClicked() {
-    document.querySelectorAll('.numbers').forEach(button => {
+    document.querySelectorAll('.button').forEach(button => {
       button.classList.remove('click');
     });
     
@@ -53,148 +53,22 @@ function buttonClicked() {
         this.classList.remove('click');
     }, "50")
   }
-  document.querySelectorAll('.numbers').forEach(button => {
+  document.querySelectorAll('.button').forEach(button => {
     button.onclick = buttonClicked;
   });
 
-//Func buttons clicked
+//number buttons adds number value
 
-function buttonClicked() {
-    document.querySelectorAll('.func').forEach(button => {
-      button.classList.remove('click');
-    });
-    
-    this.classList.add('click');
-    setTimeout(() => {
-        this.classList.remove('click');
-    }, "50")
-  }
-  document.querySelectorAll('.func').forEach(button => {
-    button.onclick = buttonClicked;
-  });
+const numberButtons = document.querySelectorAll('.numbers');
 
+numberButtons.forEach(item => {
+    item.addEventListener('click', () => {
+        if(display.textContent.length >= 6){
+            display.textContent = ' ERROR';
+        }else {
+        
+        display.textContent +=item.textContent 
+        }
+    } )
+} )
 
-//button 0
-const zero = document.querySelector('#bZero');
-zero.addEventListener('click', (number) => {
-    if(display.textContent.length >= 6){
-        display.textContent = '    ERROR';
-    }else {
-    
-    display.textContent +=zero.textContent 
-    }
-} )
-//button 1
-const one = document.querySelector('#bOne');
-one.addEventListener('click', (number) => {
-    
-    if(display.textContent.length >= 6){
-        display.textContent = '    ERROR';
-    }else {
-    
-    display.textContent +=one.textContent 
-    }
-    
-} )
-//button 2
-const two = document.querySelector('#bTwo');
-two.addEventListener('click', (number) => {
-    
-    if(display.textContent.length >= 6){
-        display.textContent = '    ERROR';
-    }else {
-    
-    display.textContent +=two.textContent 
-    }
-    
-} )
-//button 3
-const three = document.querySelector('#bThree');
-three.addEventListener('click', (number) => {
-    
-    if(display.textContent.length >= 6){
-        display.textContent = '    ERROR';
-    }else {
-    
-    display.textContent +=three.textContent 
-    }
-    
-} )
-//button 4
-const four = document.querySelector('#bFour');
-four.addEventListener('click', (number) => {
-    
-    if(display.textContent.length >= 6){
-        display.textContent = '    ERROR';
-    }else {
-    
-    display.textContent +=four.textContent 
-    }
-    
-    
-} )
-//button 5
-const five = document.querySelector('#bFive');
-five.addEventListener('click', (number) => {
-    
-    if(display.textContent.length >= 6){
-        display.textContent = '    ERROR';
-    }else {
-    
-    display.textContent +=five.textContent 
-    }
-    
-    
-} )
-//button 6
-const six = document.querySelector('#bSix');
-six.addEventListener('click', (number) => {
-    
-    if(display.textContent.length >= 6){
-        display.textContent = '    ERROR';
-    }else {
-    
-    display.textContent +=six.textContent 
-    }
-    
-    
-} )
-//button 7
-const seven = document.querySelector('#bSeven');
-seven.addEventListener('click', (number) => {
-    
-    if(display.textContent.length >= 6){
-        display.textContent = '    ERROR';
-    }else {
-    
-    display.textContent +=seven.textContent 
-    }
-    
-    
-} )
-//button 8
-const eight = document.querySelector('#bEight');
-eight.addEventListener('click', (number) => {
-    
-    if(display.textContent.length >= 6){
-        display.textContent = '    ERROR';
-    }else {
-    
-    display.textContent +=eight.textContent 
-    }
-    
-    
-} )
-//button 9
-const nine = document.querySelector('#bNine');
-nine.addEventListener('click', (number) => {
-    
-    if(display.textContent.length >= 6){
-        display.textContent = '    ERROR';
-    }else {
-    
-    display.textContent +=nine.textContent 
-    }
-    
-    
-} )
